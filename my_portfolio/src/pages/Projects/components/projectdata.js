e3331b7a8ba0f8c569cc55c7b34a16486fd21661
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-11 04:01:17 -0400

    updated projects

--- my_portfolio/src/pages/Projects/components/projectdata.js
+++ my_portfolio/src/pages/Projects/components/projectdata.js
@@ -1,37 +1,36 @@
-
 import hotel from "../../../images/hotel.PNG";
 import movie from "../../../images/movie.PNG";
 import ShareIt from "../../../images/shareit.PNG";
 const proj_data = [
   {
-    id:1,
+    id: 1,
     name: "Beach-Resorts",
     link: "https://ecr-resorts-localdb-reactproj.netlify.app/",
     desc:
       "A simple web app that helps users to choose best-qualified rooms. Users can choose based on different room types and price ranges. This app uses simple local storage",
-    git:"https://github.com/saiduddukuri411/Beach-Resort-project",
-    img:hotel,
-    lang:['React','ES6+','HTML & SASS']
+    git: "https://github.com/saiduddukuri411/Beach-Resort-project",
+    img: hotel,
+    lang: ["React", "ES6+", "HTML & SASS"],
   },
   {
-    id:2,
+    id: 2,
     name: "MovieGoogler",
     link: "https://react-movie-googler.netlify.app/",
     desc:
       "A web app for visualizing complete reviews for a movie. This uses live data provided by MovieDB API",
-    git:"https://github.com/saiduddukuri411/MovieGoogler",
-    img:movie,
-    lang:['React','ES6+','MovieDB-API']
+    git: "https://github.com/saiduddukuri411/MovieGoogler",
+    img: movie,
+    lang: ["React", "ES6+", "MovieDB-API"],
   },
   {
-    id:3,
+    id: 3,
     name: "ShareIt",
-    link: "https://share-it.netlify.app/",
+    link: "https://shareit-2ff49.web.app/",
     desc:
-      "This web app is under development!",
-    git:"https://github.com/saiduddukuri411/share_place",
-    img:ShareIt,
-    lang:['React','ES6+','Express','MongoDB']
+      "A complete web app to share your favorite places.This was built on MERN. You can also peek through other user-profiles and their favorite places. Share your favorites on share-it.",
+    git: "https://github.com/saiduddukuri411/share_place",
+    img: ShareIt,
+    lang: ["React" , "Express", "MongoDB","Firebase"],
   },
 ];
-export default proj_data
+export default proj_data;
